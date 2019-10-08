b9a98601e15a61bb4c9f0a7363d5ba9156d1911d
Darrian Soto <ddog350@gmail.com>
2019-10-08 21:43:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -25,7 +25,6 @@ answer
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
  		{
@@ -42,6 +41,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+answer = programmer_hash[:grace_hopper][:known_for]
 end
 
 def dennis_ritchies_language
